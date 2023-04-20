7a411ea4046f54e6f13073c95132f05c5511cc83
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-04-20 21:58:17 +0300

    update

--- TheMovieDatabaseApp/MovieService/MovieRepositoryLib/src/main/java/nuricanozturk/dev/movie/data/config/RestTemplateConfig.java
+++ TheMovieDatabaseApp/MovieService/MovieRepositoryLib/src/main/java/nuricanozturk/dev/movie/data/config/RestTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
 @Configuration
 public class RestTemplateConfig
 {
